393644bc80076d8e4733eede70f27963283cd7be
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-02-13 08:05:24 +0900

    Initial commit
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

---
+++ lib/whoami.ex
@@ -0,0 +1,18 @@
+defmodule Whoami do
+  @moduledoc """
+  Documentation for Whoami.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Whoami.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/whoami_test.exs
@@ -0,0 +1,8 @@
+defmodule WhoamiTest do
+  use ExUnit.Case
+  doctest Whoami
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
