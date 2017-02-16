cce7a4f3a938ad81ddc8bef55c9c5045dc364458
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-02-17 02:18:29 +0900

    Add Heroku build settings
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- lib/router.ex
+++ lib/router.ex
@@ -15,6 +15,15 @@ defmodule Whoami.Router do
   end
 
   def start_link do
-    {:ok, _} = Plug.Adapters.Cowboy.http(Whoami.Router, [])
+    {:ok, _} = Plug.Adapters.Cowboy.http(Whoami.Router, [], port: get_port())
+  end
+
+  defp get_port() do
+    port_env_variable = System.get_env("PORT")
+    if port_env_variable do
+      String.to_integer(port_env_variable)
+    else
+      4000
+    end
   end
 end
